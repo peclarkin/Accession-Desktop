0228573fbd0b445e1ea8f95fc68970aebe0240f2
peclarkin <patrick.clarkin@metaswitch.com>
2013-02-11 15:06:47 +0000

    Update Repo

--- metamania/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if [%1] == [] echo no parameters, assume just run
-if NOT [%2] == [] pushd plugins\%2
-if NOT [%1] == [] call ant %1
-if NOT [%2] == [] popd
-if NOT [%2] == [] call ant make-brands
-pushd dist\accession
-call ant run
-popd
